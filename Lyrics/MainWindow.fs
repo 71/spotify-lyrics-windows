61deac4e786a1cf40e55a0cd85ac66bcf55943a0
Grégoire Geis <git@gregoirege.is>
2019-05-28 12:19:34 +0900

    Added lyrics interpolation and bug fixes.

--- Lyrics/MainWindow.fs
+++ Lyrics/MainWindow.fs
@@ -29,6 +29,8 @@ type internal MainWindow() as this =
         |> Style.build
 
     let spotify = new Spotify()
+    let mutable lastPosition = 0
+    let mutable guessedMilliseconds = 0
 
     let currentTimeText = TextBlock()
     let titleText       = TextBlock()
@@ -37,6 +39,30 @@ type internal MainWindow() as this =
     let captions = List<Musixmatch.Subtitle>()
     let captionsList = ItemsControl()
     let mutable activeCaption = null :> TextBlock
+
+    let positionChanged position ms =
+        this.Dispatcher.InvokeSafe <| fun _ ->
+            let min, sec = position / 60, position % 60
+
+            currentTimeText.Text <- sprintf "%d:%02d" min sec
+
+            if captionsList.Items.Count <> 0 then
+                match activeCaption with
+                | null -> ()
+                | caption -> caption.Style <- captionStyle
+
+                let position = float position + ms
+                let captionIndex = captions.FindLastIndex(fun x -> x.Time <= position)
+                let captionIndex = if captionIndex = -1 then captions.Count - 1 else captionIndex
+
+                if captionIndex <> -1 && captionIndex < captionsList.Items.Count then
+                    match captionsList.Items.[captionIndex] with
+                    | :? TextBlock as item ->
+                        activeCaption <- item
+                        item.Style <- activeCaptionStyle
+                    | _ -> ()
+                else
+                    activeCaption <- null
 
     do
         // Setup window chrome
@@ -85,34 +111,28 @@ type internal MainWindow() as this =
                         ignore <| captionsList.Items.Add(caption)
             }
 
+        let mutable timeChanged = false
+
         spotify.TimeChanged.Add <| fun position ->
-            this.Dispatcher.InvokeSafe <| fun _ ->
-                let min, sec = position / 60, position % 60
+            lastPosition <- position
+            timeChanged  <- true
 
-                currentTimeText.Text <- sprintf "%d:%02d" min sec
-
-                if captions.Count <> 0 then
-                    match activeCaption with
-                    | null -> ()
-                    | caption -> caption.Style <- captionStyle
-
-                    let position = float position
-                    let captionIndex = captions.FindLastIndex(fun x -> x.Time <= position)
-                    let captionIndex = if captionIndex = -1 then captions.Count - 1 else captionIndex
-
-                    match captionsList.Items.[captionIndex] with
-                    | :? TextBlock as item ->
-                        activeCaption <- item
-                        item.Style <- activeCaptionStyle
-                    | _ -> ()
-
+            positionChanged position 0.
 
         // Trigger a refresh of the currently playing song every 500ms
         let timer = new Timer()
 
         timer.Interval <- 200.0
         timer.Elapsed.Add <| fun _ ->
+            timeChanged <- false
+
             spotify.Update()
+
+            if timeChanged then
+                guessedMilliseconds <- 0
+            else
+                guessedMilliseconds <- min (guessedMilliseconds + 200) 900
+                positionChanged lastPosition (float guessedMilliseconds / 1000.)
 
         timer.Start()
 
